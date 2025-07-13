ad26257e0f90cdb04555b30b172b3af2ad26da33
zliu2252-jim <zliu2252@wisc.edu>
2025-07-13 17:38:53 -0500

    Update OutputCode.java

--- app/src/main/java/org/example/OutputCode.java
+++ app/src/main/java/org/example/OutputCode.java
@@ -10,13 +10,13 @@ public class OutputCode {
         // Example 1: Merge two lists of integers in alternating order
         List<Integer> nums1 = List.of(1, 3, 5, 7);
         List<Integer> nums2 = List.of(2, 4, 6, 8);
-        List<Integer> mergedNumbers = ListUtils.zip(nums1, nums2);
+        List<Integer> mergedNumbers = App.zip(nums1, nums2);
         System.out.println(mergedNumbers);
 
         // Example 2: Merge two lists of strings in alternating order
         List<String> colors1 = List.of("Red", "Green", "Blue");
         List<String> colors2 = List.of("White", "Black", "Orange", "Pink", "Fuschia");
-        List<String> mergedWords = ListUtils.zip(colors1, colors2);
+        List<String> mergedWords = App.zip(colors1, colors2);
         System.out.println(mergedWords);
     }
 }
